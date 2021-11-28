b73d236b480ebccd6e7b87a94d4b0b441a0abdd1
Sam <76275133+gn00866254@users.noreply.github.com>
2021-11-28 11:13:05 +0900

    11/12 validate data

--- samflix/includes/classes/Account.php
+++ samflix/includes/classes/Account.php
@@ -11,18 +11,76 @@ class Account{
         
     }
     
-    public function validateFirstName($fn){
+    public function register($fn,$ln,$un,$em,$em2,$pw,$pw2){
+        $this->validateFirstName($fn);
+        $this->validateLastName($ln);
+        $this->validateUsername($un);
+        $this->validateEmails($em,$em2);
+        $this->validatePasswords($pw,$pw2);
+
+    }
+    
+    private function validateFirstName($fn){
         if (strlen($fn)<2 || strlen($fn) > 25) {
-            array_push($this->errorAttay , "First name wrong length");
+            array_push($this->errorArray , Constants::$firstNameCharacters);
         }
     }
     
-    public function getError($error){
-        if(in_array($error,$this->errorArray)){
-            return $error;
+    private function validateLastName($ln){
+        if (strlen($ln)<2 || strlen($ln) > 25) {
+            array_push($this->errorArray , Constants::$lastNameCharacters);
         }
     }
 
+    private function validateUsername($un){
+        if (strlen($un)<2 || strlen($un) > 50) {
+            array_push($this->errorArray , Constants::$usernameCharacters);
+            return;
+        }
+        //Object of class PDOStatement 実行したいSQL文をセットする
+        $query=$this->con->prepare("SELECT * FROM users WHERE username=:un");
+        //SQLに対してパラメーターをセットする。
+        $query->bindValue(":un",$un);
+        //実際にSQLを実行する。
+        $query->execute();
+        //0じゃない。つまり存在する
+        if ($query->rowCount() != 0){
+            array_push($this->errorArray , Constants::$usernameTaken);
+        }
+    }
 
+    private function validateEmails($em,$em2){
+        if ($em != $em2){
+            array_push($this->errorArray, Constants::$emailsDontMatch);
+            return;
+        }
+        if(!filter_var($em,FILTER_VALIDATE_EMAIL)){
+            array_push($this->errorArray, Constants::$emailInvalid);
+            return;
+        }
+
+        $query=$this->con->prepare("SELECT * FROM users WHERE email=:em");
+        $query->bindValue(":em",$em);
+        $query->execute();
+        if ($query->rowCount() != 0){
+            array_push($this->errorArray , Constants::$emailTaken);
+        }
+    }
+
+    private function validatePasswords($pw,$pw2){
+        if($pw!=$pw2){
+            array_push($this->errorArray, Constants::$passwordsDontMatch);
+            return;
+        }
+        if (strlen($pw)<5 || strlen($pw) > 100) {
+            array_push($this->errorArray , Constants::$passwordLength);
+        }
+
+    }
+    public function getError($error){
+        if(in_array($error,$this->errorArray)){
+            return "<span class='errorMessage'>$error</span>";
+        }
+    }
 }
 ?>
--- samflix/includes/config.php
+++ samflix/includes/config.php
@@ -5,8 +5,8 @@ session_start();
 date_default_timezone_set("Asia/Tokyo");
 
 try {
-    //インスタンスを作成することで接続を確率する。
-    $con = new PDO("mysql:dbname=samflix;host=localhost","root","");
+    //インスタンスを作成することで接続を確率する。PDO object
+    $con = new PDO("mysql:dbname=samflix+;host=localhost","root","");
     $con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
 }
 catch (PDOException $e){
