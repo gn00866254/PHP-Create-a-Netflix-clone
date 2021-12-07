1e9d5eb5c74e8a09d72c42318c6b0bd673c59663
Sam <shiseihuang@gmail.com>
2021-12-07 20:24:46 +0900

    chage variable name

--- samflix/includes/classes/Entity.php
+++ samflix/includes/classes/Entity.php
@@ -1,11 +1,23 @@
 <?php 
 class Entity{
     
-    private $con,$input;
+    private $con,$sqlData;
 
     public function __construct($con,$input){
         $this->con = $con;
-        $this->input = $input;
+
+        //ランダムに抽出することにより、arrayであれば
+        if (is_array($input)){
+            $this->sqlData = $input;
+        //そうでなければ
+        }else{
+            $query = $this->con->prepare("SELECT * FROM entities WHERE id=:d");
+            $query->bindValue(":d",$input);
+            $query->excute();
+            
+            $this->sqlData = $query->fetch(PDO::FETCH_ASSOC);
+        }
+        
 
 
     }
